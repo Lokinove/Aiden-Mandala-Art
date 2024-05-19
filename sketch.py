77a0b34400f10cb463f48d33b3be4fd7c7b0e91f
Lokinove <157891310+Lokinove@users.noreply.github.com>
2024-05-19 10:56:22 +0000

    commit

--- sketch.py
+++ sketch.py
@@ -1,9 +1,29 @@
 from p5 import *
 
 def setup():
-  createCanvas(windowWidth,windowHeight)
-  background("black")
-
+  createCanvas(windowWidth, windowHeight)
+  background(0)
+x = 12
+hue = 0
 
 def draw():
-  drawTickAxes()
+  global x, hue
+  # background(0)
+  translate(width/2, height/2)
+  # drawTickAxes()
+  if mouseIsPressed:
+    for i in range(x):
+      stroke(hue, 100, 100)
+      strokeWeight(5)
+      point(mouseX - width/2, mouseY - height/2)
+      rotate(360/x)
+      hue += 18
+      if hue > 360:
+        hue = 0
+  # text(keyCode, 0, 0)
+
+def keyPressed():
+  if keyCode == 76:
+    save("hello, you have been spear phished ")
+  if keyCode == 82:
+    background(0)
